bb5d1bbb526d026a8d2be7fc7a8fb051f23de2af
Raj Kumar Singh <149702946+rajsingh116@users.noreply.github.com>
2026-04-27 12:45:17 +0530

    Add files via upload

---
+++ 23BCS11393_LABEST/Q3.Distinct Subset/ds.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(int index, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans) {
+        ans.push_back(ds);
+
+        for(int i = index; i < nums.size(); i++) {
+
+            if(i > index && nums[i] == nums[i-1]) continue;
+
+            ds.push_back(nums[i]);
+            solve(i + 1, nums, ds, ans);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> findSubsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        sort(nums.begin(), nums.end()); 
+        solve(0, nums, ds, ans);
+
+        return ans;
+    }
+};
